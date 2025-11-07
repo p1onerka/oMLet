466b7545aedac318b476675eb38e586e0ed861aa
sofyak0zyreva <k81sofia@gmail.com>
2025-11-07 20:29:46 +0300

    feat: draft of gc, introduce boxed values
    Signed-off-by: sofyak0zyreva <k81sofia@gmail.com>

--- OMLet/many_tests/typed/build_and_run.sh
+++ OMLet/many_tests/typed/build_and_run.sh
@@ -4,17 +4,19 @@ SRC="$1"
 COMPILER="../../../bin/omlet.exe"
 RUNTIME="../../../lib/runtime.c"
 GC="../../../lib/gc.c"
+BOX="../../../lib/boxing.c"
 CALLF="../../../lib/callf.s"
 
 "$COMPILER" -fromfile "$SRC" | riscv64-linux-gnu-as -march=rv64gc -o temp.o -
 riscv64-linux-gnu-gcc "$RUNTIME" -c -o runtime.o
 riscv64-linux-gnu-gcc "$GC" -c -o gc.o
+riscv64-linux-gnu-gcc "$BOX" -c -o box.o
 riscv64-linux-gnu-gcc "$CALLF" -c -o callf.o
-riscv64-linux-gnu-gcc temp.o runtime.o gc.o callf.o -nostartfiles -o binary.exe
+riscv64-linux-gnu-gcc temp.o runtime.o gc.o box.o callf.o -nostartfiles -o binary.exe
 qemu-riscv64 -L /usr/riscv64-linux-gnu binary.exe
 
 EXIT_CODE=$?
 
-rm -f temp.o runtime.o gc.o callf.o factorial.exe
+rm -f temp.o runtime.o gc.o box.o callf.o factorial.exe
 
 exit $EXIT_CODE

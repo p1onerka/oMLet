83580c717aeecf3e17a6686c171398a98685de43
sofyak0zyreva <k81sofia@gmail.com>
2025-11-10 02:54:49 +0300

    new: new attempt at gc implementation
    Signed-off-by: sofyak0zyreva <k81sofia@gmail.com>

--- OMLet/lib/gc.h
+++ OMLet/lib/gc.h
@@ -3,4 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-void *omlet_malloc(size_t size);
+typedef enum {
+  T_UNBOXED = 0,
+  T_CLOSURE = 1,
+} tag_t;
+
+void *omlet_malloc(size_t size, tag_t tag);
